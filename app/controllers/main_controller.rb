d5321c9f61be8b254b90fbee63068ba506f95038
tekniklr <tekniklr@tekniklr.com>
2012-02-02 02:48:38 -0800

    Display page title in html head title

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,31 +7,37 @@ class MainController < ApplicationController
   end
   
   def services
+    @page_title = "Services"
     blurb = Blurb.find_by_name("Services")
     @services = !blurb.blank? ? blurb.content : ''
   end
   
   def forms
+    @page_title = "Forms"
     blurb = Blurb.find_by_name("Forms")
     @forms = !blurb.blank? ? blurb.content : ''
   end
   
   def op_instructions
+    @page_title = "Pre/Post Op"
     blurb = Blurb.find_by_name("Pre/Post Op")
     @op_instructions = !blurb.blank? ? blurb.content : ''
   end
   
   def about
+    @page_title = "About"
     blurb = Blurb.find_by_name("About")
     @about = !blurb.blank? ? blurb.content : ''
   end
   
   def donate
+    @page_title = "How can you help?"
     blurb = Blurb.find_by_name("How you can help")
     @donate = !blurb.blank? ? blurb.content : ''
   end
   
   def contact
+    @page_title = "Contact"
     blurb = Blurb.find_by_name("Contact")
     @contact = !blurb.blank? ? blurb.content : ''
   end
