5982113283877a9986860161e8c29293a0ab491d
tekniklr <tekniklr@tekniklr.com>
2012-02-23 21:01:51 -0800

    Layout is fluid again.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,34 @@ module ApplicationHelper
   def blurb(raw_blurb)
     sanitize raw_blurb, :tags => %w(a strong em u br cite span ol ul li p b i)
   end
-    
+  
+  # haml ie/css hacks: https://github.com/sporkd/compass-html5-boilerplate/
+  def ie_tag(name=:body, attrs={}, &block)
+    attrs.symbolize_keys!
+    haml_concat("<!--[if lt IE 7]> #{ tag(name, add_class('ie6', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 7]>    #{ tag(name, add_class('ie7', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 8]>    #{ tag(name, add_class('ie8', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if gt IE 8]><!-->".html_safe)
+    haml_tag name, attrs do
+      haml_concat("<!--<![endif]-->".html_safe)
+      block.call
+    end
+  end
+  def ie_html(attrs={}, &block)
+    ie_tag(:html, attrs, &block)
+  end
+  def ie_body(attrs={}, &block)
+    ie_tag(:body, attrs, &block)
+  end
+  
+  private
+
+  def add_class(name, attrs)
+    classes = attrs[:class] || ''
+    classes.strip!
+    classes = ' ' + classes if !classes.blank?
+    classes = name + classes
+    attrs.merge(:class => classes)
+  end
+  
 end
