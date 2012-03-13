309e59ac1bc9e57682f4b040b36d08120e6eb034
tekniklr <tekniklr@tekniklr.com>
2012-03-13 02:00:04 -0700

    Stop recaptcha from bitching in tests.

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 # http://lowcostspayneuteraz.org
 Recaptcha.configure do |config|
-  config.public_key  = ENV['RECAPTCHA_PUBLIC']
-  config.private_key = ENV['RECAPTCHA_PRIVATE']
+  config.public_key  = Rails.env.test? ? "asdfghjklwertyuiop" : ENV['RECAPTCHA_PUBLIC']
+  config.private_key = Rails.env.test? ? "asdfghjklwertyuiop" : ENV['RECAPTCHA_PRIVATE']
 end
