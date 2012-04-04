a2b38e2311832cc714cc31d0c3710a4b13cff805
tekniklr <tekniklr@tekniklr.com>
2012-04-04 01:02:11 -0700

    Trying to fix ckeditor precompilation error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ MobileClinic::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( modernizr-1.7.min.js )
+  config.assets.precompile += %w( modernizr-1.7.min.js ckeditor/* )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
