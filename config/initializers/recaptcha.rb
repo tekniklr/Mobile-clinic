1192430120c9d6a98084fce8c523fb306765b725
tekniklr <tekniklr@tekniklr.com>
2014-11-18 13:29:39 -0800

    Separate recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
-# http://lowcostspayneuteraz.org
+# http://lowcostspayneuterclinicflagstaff.org
 Recaptcha.configure do |config|
   config.public_key  = (Rails.env.test? || Rails.env.development?) ? "recaptcha_this" : ENV['RECAPTCHA_PUBLIC']
   config.private_key = (Rails.env.test? || Rails.env.development?) ? "recaptcha_this_secretly" : ENV['RECAPTCHA_PRIVATE']
