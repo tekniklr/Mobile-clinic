911904ab503e838d3c7d6751cf7c98c5d430d34d
tekniklr <tekniklr@tekniklr.com>
2014-11-18 13:31:43 -0800

    Use own smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,6 @@ MobileClinic::Application.configure do
     :authentication       => 'plain',
     :enable_starttls_auto => true
   }
-  config.action_mailer.default_url_options = { :host => 'spayneuterclinic.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'spayneuterclinic-location-two.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
 end
