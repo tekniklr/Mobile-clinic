4fe02e3da9465bf4f4b92e6117bb31243e17cecf
tekniklr <tekniklr@tekniklr.com>
2014-11-18 14:55:16 -0800

    Trying to debug logging in via facebook for new site

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
   
   def login
+    logger.info "************ Login page requested"
     page_title 'login', true
     if Rails.env.development?
+      logger.info "************ dev enviromnent - whatevs"
       # just log in as the first user
       user = User.first
       user or raise UserNotAuthorized
@@ -18,9 +20,13 @@ class SessionsController < ApplicationController
   end
   
   def validate
+    logger.info "************ validating login"
     auth = request.env["omniauth.auth"]
+    logger.info "************ provider: #{auth["provider"]}"
+    logger.info "************ uid: #{auth["uid"]}"
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
     user or raise UserNotAuthorized
+    logger.info "************ user found: #{user.handle}!"
     session[:user_id] = user.id
     flash[:notice] = 'Signed in!'
     redirect_to root_url
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,7 @@ MobileClinic::Application.configure do
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
   #config.logger = Logger.new(Rails.root.join("log",Rails.env + ".log"),10,5*1024*1024)
+  config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
