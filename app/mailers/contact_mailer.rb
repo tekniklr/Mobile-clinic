7de823caa0f3295959b839bd5ab4d738668af641
tekniklr <tekniklr@tekniklr.com>
2015-06-16 16:07:33 -0700

    Upgrade to address recent vulnerabilities

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ActionMailer::Base
   def contact(contact)
     @contact = contact
     mail(
-      :to => Rails.env.development? ? 'tsolow@tekniklr.com' : 'Taylor Williams <drtsheltervet@gmail.com>',
+      :to => Rails.env.development? ? 'apps@tekniklr.com' : 'Taylor Williams <drtsheltervet@gmail.com>',
       :from => "#{contact.name} <#{contact.email}>",
       :subject => "Contact form submitted (Kingman)"
     )
